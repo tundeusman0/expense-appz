64386337b69efa0230a29a751503f5098191f691
tundeUsman <tundeusman0@gmail.com>
2019-07-24 10:38:28 +0100

    configure for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require("path");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env) =>{
     const isProduction = env === 'production';
     return {
+        mode: isProduction ? 'production' : 'development',
         entry: './src/app.js',
         output: {
             path: path.join(__dirname, 'public', 'dist'),
             filename: "bundle.js",
+            chunkFilename:'chunky.js',
         },
         module: {
             rules: [{
@@ -17,24 +21,33 @@ module.exports = (env) =>{
                 }, {
                 test: /\.s?css$/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                        MiniCssExtractPlugin.loader,
+                        { loader: 'css-loader', options: { url: false, sourceMap: true } },
+                        { loader: 'sass-loader', options: { sourceMap: true } }
+                    ],
             }]
 
         },
-        devtool: 'cheap-module-eval-source-map',
-        // devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             publicPath: '/dist/',
             historyApiFallback: true,
         },
-        // plugins: [
-        //     new HtmlWebpackPlugin({
-        //         template: 'public/index.html'
-        //     })
-        // ]
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "style.css"
+            }),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({ 
+                template: './public/index.html',
+                filename: './index.html' 
+            }),
+        ],
+        performance: {
+            hints: false,
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000,
+        }
     }
 }
