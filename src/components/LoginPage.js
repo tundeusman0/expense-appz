908cc4a706815654bbf0ef5c1b82b00d26b9cd7d
tundeUsman <tundeusman0@gmail.com>
2019-07-30 14:14:22 +0100

    added footer

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ export const LoginPage = ({ startLogin }) =>(
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
-            <p>Get your expenses under control buy keeping track.</p>
+            <p>Get your expenses under control by keeping track.</p>
             <button className="button" onClick={startLogin}>Login with Google</button>
         </div>
     </div>
